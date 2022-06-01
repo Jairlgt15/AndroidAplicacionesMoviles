088967d46fdf9c2f11c9d8e284d2e87ce8681fed
JG <ludwing.gomez@epn.edu.ec>
2022-05-31 21:54:45 -0500

    toast correcto, habia que elegir con ctrl p la estructura, lo de plomo (context) se pone solito

--- app/src/main/java/com/jairgomez/miercoles/MainActivity.kt
+++ app/src/main/java/com/jairgomez/miercoles/MainActivity.kt
@@ -8,7 +8,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Toast.makeText(context: this, text: "Hola mundo", Toast.LENGTH_LONG)
-        .show()
+        Toast.makeText(this,"Hola mundo", Toast.LENGTH_LONG )
+            .show()
     }
 }
